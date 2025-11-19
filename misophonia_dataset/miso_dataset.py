8f09c6027c85abc777c003abb0e8c43cfbb38d6a
Tonio Ermakoff <termakoff@gmail.com>
2025-11-19 04:16:40 -0600

    added FSD50Keval class

--- misophonia_dataset/miso_dataset.py
+++ misophonia_dataset/miso_dataset.py
@@ -256,6 +256,8 @@ class ESC50(SourceData):
         shutil.rmtree(self.dir_path)
         shutil.rmtree("esc50-extracted.json")
 
+    def __str__(self) -> str:
+        return "ESC50 Dataset"
 
 class FSD50K(SourceData):
     """
@@ -342,8 +344,10 @@ class FSD50K(SourceData):
         # Save the dataset path to a JSON in case a new FSD50K class is made and dataset is present
         if os.path.isfile("fsd50k-zip.txt"):
             os.rename("fsd50k-zip.txt", "fsd50k-extracted.json")
-        with open("fsd50k-extracted.json", "w") as f:
-            json.dump({"Path": extracted_path}, f, indent=4)
+        with open("fsd50k-extracted.json", "r+") as f:
+            data = json.load(f)
+            data["Path"] = extracted_path
+            json.dump(data, f, indent=4)
 
         return Path(extracted_path)
 
@@ -421,7 +425,153 @@ class FSD50K(SourceData):
     def delete(self) -> None:
         shutil.rmtree(self.path)
         shutil.rmtree("fsd50k-extracted.json")
+    
+    def __str__(self) -> str:
+        return "FSDK50 Dataset"
 
+
+class FSD50K_eval(SourceData):
+    """
+        Class for the FSD50K eval set. Data is downloaded from Zenodo, https://zenodo.org/records/4060432.
+        When downloading and unzipping the dataset, txt files are generated to track progress.
+        Controls, triggers, and backgrounds are sampled from FSD50K.
+    """
+    def __init__(self, mapping: Path, backgrounds: Path, save_dir: Path) -> None:
+        with open(mapping, "r") as f:
+            self.mapping = json.load(f)
+
+        with open(backgrounds, "r") as f:
+            self.backgrounds = json.load(f)["Backgrounds"]
+
+        self.path = self.download_data(save_dir)
+
+        fsd50k_eval = self.get_metadata(self.path)
+        self.metadata = self.get_samples(fsd50k_eval)
+
+    def download_data(self, save_dir: Path) -> Path:
+        """
+        Downloads, combines, and extracts FSD50K eval dataset from Zenodo using multiple threads, with resume support.
+        Params:
+            save_dir (Path): Path to save the dataset
+        Returns:
+            full path of saved dataset
+        """
+        # FSD50K eval requires FSD50K dev to be downloaded first, particularly for metadata.
+        if not os.path.exists("fsd50k-extracted.json"):
+            raise FileNotFoundError("Please download FSD50K dev dataset before downloading the eval dataset.")
+
+        with open("fsd50k-extracted.json", "r") as f:
+                data = json.load(f)
+                if "evalPath" in data.keys():
+                    print(f"FSD50K eval dataset has already been downloaded and unzipped at {save_dir}")
+                    return data["evalPath"]
+
+        urls = [
+            (
+                "https://zenodo.org/records/4060432/files/FSD50K.eval_audio.zip?download=1",
+                "6fa47636c3a3ad5c7dfeba99f2637982",
+            ),
+            (
+                "https://zenodo.org/records/4060432/files/FSD50K.eval_audio.z01?download=1",
+                "3090670eaeecc013ca1ff84fe4442aeb ",
+            ),
+        ]
+
+        os.makedirs(save_dir, exist_ok=True)
+
+        if not os.path.exists("fsd50k-eval-zip.txt"):
+            with ThreadPoolExecutor(max_workers=2) as executor:
+                zip_files = list(executor.map(lambda url_hash: download_file(url_hash[0], url_hash[1], save_dir), urls))
+
+            print(zip_files[0])
+
+            with open("fsd50k-eval-zip.txt", "w") as f:
+                pass  # to track zip + extraction progress
+        else:
+            print("FSD50K eval dataset has already been downloaded. Proceeding to extraction.")
+
+        print("\nUnzipping FSD50K eval dataset...")
+        subprocess.run(["7z", "x", zip_files[0], f"-o{save_dir}"], check=True)
+
+        # First remove component zip files
+        for file in zip_files:
+            os.remove(file)
+
+        extracted_path = os.path.join(save_dir, "FSD50K.eval_audio")
+
+        # Save the dataset path to a JSON in case a new FSD50K_eval class is made and dataset is present
+        if os.path.isfile("fsd50k-eval-zip.txt"):
+            os.remove("fsd50k-eval-zip.txt")
+
+        with open("fsd50k-extracted.json", "r+") as f:
+            data = json.load(f)
+            data["evalPath"] = extracted_path
+            json.dump(data, f, indent=4)
+
+        return Path(extracted_path)
+
+    def get_metadata(self, extracted_path: Path) -> pd.DataFrame:
+        """
+        Downloads FSD50K eval metadata from zenodo and saves as a dataframe.
+
+        """
+        # Check if metadata has already been downloaded
+        if os.path.exists("fsd50k-extracted.json"):
+            with open("fsd50k-extracted.json", "r") as f:
+                data = json.load(f)
+                if "evalMeta" in data.keys():
+                    return pd.read_csv(data["evalMeta"])
+        else:
+            raise FileNotFoundError("Please download FSD50K dev dataset before downloading the eval metadata.")
+
+        metadata_path = os.path.join(extracted_path, "FSD50K.metadata", "collection", "collection_eval.csv")
+
+        # Save metadata path so that if new class is instantiated metadata can easily be retrieved.
+        with open("fsd50k-extracted.json", "r+") as f:
+            data = json.load(f)
+            data["evalMeta"] = metadata_path
+            json.dump(data, f, indent=4)
+
+        return pd.read_csv(metadata_path)
+
+    def get_samples(self, fsd50k_eval: pd.DataFrame) -> pd.DataFrame:
+        """
+        Collects triggers, controls, backgrounds from full FSD50K.eval set using predefined class mappings.
+        Removes all unused sound samples.
+        Params:
+            fsd50k_eval (dataframe): full metadata for dataset
+        Returns:
+            new metadata dataframe including only the collected sound samples.
+        """
+        trigger_classes = [k for k in self.mapping["Trigger"].keys()]
+        control_classes = [k for k in self.mapping["Control"].keys()]
+
+        conditions = [
+            fsd50k_eval["labels"].isin(control_classes),
+            fsd50k_eval["labels"].isin(trigger_classes),
+            fsd50k_eval["labels"].isin(self.backgrounds),
+        ]
+        choices = [0, 1, 2]
+
+        # Only keep rows of collected samples
+        fsd50k_eval["isTrig"] = np.select(conditions, choices, default=-1)
+        fsd50k_eval = fsd50k_eval[fsd50k_eval["isTrig"] >= int(0)].copy()
+
+        # Update label mapping only for Trigger rows
+        fsd50k_eval.loc[fsd50k_eval["isTrig"] == 1, "labels"] = fsd50k_eval.loc[fsd50k_eval["isTrig"] == 1, "labels"].apply(
+            lambda x: self.mapping["Trigger"][str(x)]["foams_mapping"]
+        )
+
+        # Fix rename syntax
+        fsd50k_eval = fsd50k_eval.rename(columns={"fname": "filename"})
+
+        return fsd50k_eval
+
+    def delete(self) -> None:
+        shutil.rmtree(self.path)
+
+    def __str__(self) -> str:
+        return "FSDK50 Eval Dataset"
 
 class FOAMS(SourceData):
     """
@@ -474,6 +624,9 @@ class FOAMS(SourceData):
     def delete(self) -> None:
         shutil.rmtree(self.path)
 
+    def __str__(self) -> str:
+        return "FOAMS Dataset"
+
 
 class MisophoniaData:
     """
@@ -481,11 +634,12 @@ class MisophoniaData:
     column and "isTrig" column
     """
 
-    def __init__(self, source_data: list[SourceData]) -> None:
+    def __init__(self, source_data: list[SourceData], test_data: list[SourceData]) -> None:
         self._source_data = source_data
+        self._test_data = test_data
         pass
 
-    def generate(self) -> None:
+    def generate(self) -> Generator[MisophoniaItem]:
         pass
 
     class MisophoniaItem:
@@ -504,4 +658,6 @@ if __name__ == "__main__":
 
     # esc50 = ESC50(mapping=esc50_mapping, save_dir=DEFAULT_DIR)
     fsd50k = FSD50K(mapping=fsd50k_mapping, backgrounds=background_classes, save_dir=DEFAULT_DIR)
+    fsd50_eval = FSD50K_eval(mapping=fsd50k_mapping, backgrounds=background_classes, save_dir=DEFAULT_DIR)
+
     # foams = FOAMS(save_dir=DEFAULT_DIR)
